81bd239c3f52c357730c3ba88e27e94b47364c41
kwoumn3 <kwoumn@gmail.com>
2020-03-22 14:59:20 -0700

    Use environment variables for firebase service account

--- routes/firebase.js
+++ routes/firebase.js
@@ -3,7 +3,29 @@ const router = express.Router();
 
 var admin = require("firebase-admin");
 
-var serviceAccount = require("/Users/ashleyt/Downloads/im-bored-d4aa9-firebase-adminsdk-ig9y1-7d573f9ed7.json");
+const FIREBASE_TYPE = process.env.FIREBASE_TYPE;
+const FIREBASE_PROJECT_ID = process.env.FIREBASE_PROJECT_ID;
+const FIREBASE_PRIVATE_KEY_ID = process.env.FIREBASE_PRIVATE_KEY_ID;
+const FIREBASE_PRIVATE_KEY = process.env.FIREBASE_PRIVATE_KEY;
+const FIREBASE_CLIENT_EMAIL = process.env.FIREBASE_CLIENT_EMAIL;
+const FIREBASE_CLIENT_ID = process.env.FIREBASE_CLIENT_ID;
+const FIREBASE_AUTH_URI = process.env.FIREBASE_AUTH_URI;
+const FIREBASE_TOKEN_URI = process.env.FIREBASE_TOKEN_URI;
+const FIREBASE_AUTH_PROVIDER_X509_CERT_URL = process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL;
+const FIREBASE_CLIENT_X509_CERT_URL = process.env.FIREBASE_CLIENT_X509_CERT_URL;
+
+const serviceAccount = {
+    "type": FIREBASE_TYPE,
+    "project_id": FIREBASE_PROJECT_ID,
+    "private_key_id": FIREBASE_PRIVATE_KEY_ID,
+    "private_key": FIREBASE_PRIVATE_KEY,
+    "client_email": FIREBASE_CLIENT_EMAIL,
+    "client_id": FIREBASE_CLIENT_ID,
+    "auth_uri": FIREBASE_AUTH_URI,
+    "token_uri": FIREBASE_TOKEN_URI,
+    "auth_provider_x509_cert_url": FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": FIREBASE_CLIENT_X509_CERT_URL
+  }
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
